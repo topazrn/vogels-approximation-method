d5a7f59619aa93a812f714705fab5f24d5920f50
TopazRN <topazsorowako2@gmail.com>
2022-08-29 20:23:52 +0700

    Create indexOfMin

---
+++ src/indexOfMin.test.ts
@@ -0,0 +1,5 @@
+import { indexOfMin } from "./indexOfMin"
+
+test("indexOfMin([12, 34, 98, 75, 64, 29, 73, 86]) is 0", () => {
+  expect(indexOfMin([12, 34, 98, 75, 64, 29, 73, 86])).toBe(0)
+})
---
+++ src/indexOfMin.ts
@@ -0,0 +1,16 @@
+export const indexOfMin = (array: number[]): number => {
+  if (array.length === 0) {
+    return -1
+  }
+
+  let tempIndex = 0
+  let tempValue = array[tempIndex]
+  for (let index = 0; index < array.length; index++) {
+    const value = array[index]
+    if (value < tempValue) {
+      tempIndex = index
+    }
+  }
+
+  return tempIndex
+}
