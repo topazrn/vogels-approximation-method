98b36dbf97a20c4cc9250b957d2f81f297a15c92
TopazRN <topazsorowako2@gmail.com>
2022-08-30 05:52:57 +0700

    Add indexOf(Max and Min) testcases

--- src/indexOfMax.test.ts
+++ src/indexOfMax.test.ts
@@ -7,3 +7,7 @@ test("indexOfMax([12, 34, 98, 75, 64, 29, 73, 86]) is 2", () => {
 test("indexOfMax([12, 34, 98, 75, 64, 29, 73, 86], [2, 3]) is 7", () => {
   expect(indexOfMax([12, 34, 98, 75, 64, 29, 73, 86], [2, 3])).toBe(7)
 })
+
+test("indexOfMax([5, 2, 2], [0]) is 1", () => {
+  expect(indexOfMax([5, 2, 2], [0])).toBe(1)
+})
--- src/indexOfMin.test.ts
+++ src/indexOfMin.test.ts
@@ -4,7 +4,10 @@ test("indexOfMin([12, 34, 98, 75, 64, 29, 73, 86]) is 0", () => {
   expect(indexOfMin([12, 34, 98, 75, 64, 29, 73, 86])).toBe(0)
 })
 
-
 test("indexOfMin([12, 34, 98, 75, 64, 29, 73, 86], [0, 2, 3]) is 5", () => {
   expect(indexOfMin([12, 34, 98, 75, 64, 29, 73, 86], [0, 2, 3])).toBe(5)
+})
+
+test("indexOfMin([2, 5, 5], [0]) is 1", () => {
+  expect(indexOfMin([2, 5, 5], [0])).toBe(1)
 })
