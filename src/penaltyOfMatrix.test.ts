0f7948637dc0dfc093daa570dbd19bfead4023a6
TopazRN <topazsorowako2@gmail.com>
2022-08-30 08:07:32 +0700

    Add test cases from byjus.com https://byjus.com/maths/vogels-approximation-method

--- src/penaltyOfMatrix.test.ts
+++ src/penaltyOfMatrix.test.ts
@@ -12,3 +12,61 @@ test("https://cdn1.byjus.com/wp-content/uploads/2022/04/Vogels-Approximation-Met
     y: [1, 1, 2],
   })
 })
+
+test("https://cdn1.byjus.com/wp-content/uploads/2022/04/Vogels-Approximation-Method-2.png", () => {
+  expect(
+    penaltyOfMatrix([
+      [3, 7, 6],
+      [7, 2, 3],
+      [2, 4, 5],
+    ])
+  ).toStrictEqual({
+    x: [1, 2, 2],
+    y: [3, 1, 2],
+  })
+})
+
+test("https://cdn1.byjus.com/wp-content/uploads/2022/04/Vogels-Approximation-Method-3.png", () => {
+  expect(
+    penaltyOfMatrix([
+      [7, 2, 3],
+      [2, 4, 5],
+    ])
+  ).toStrictEqual({
+    x: [5, 2, 2],
+    y: [1, 2],
+  })
+})
+
+test("https://cdn1.byjus.com/wp-content/uploads/2022/04/Vogels-Approximation-Method-4.png", () => {
+  expect(
+    penaltyOfMatrix([
+      [7, 2, 3],
+    ])
+  ).toStrictEqual({
+    x: [7, 2, 3],
+    y: [1],
+  })
+})
+
+test("https://cdn1.byjus.com/wp-content/uploads/2022/04/Vogels-Approximation-Method-5.png", () => {
+  expect(
+    penaltyOfMatrix([
+      [2, 3],
+    ])
+  ).toStrictEqual({
+    x: [2, 3],
+    y: [1],
+  })
+})
+
+test("https://cdn1.byjus.com/wp-content/uploads/2022/04/Vogels-Approximation-Method-6.png", () => {
+  expect(
+    penaltyOfMatrix([
+      [2],
+    ])
+  ).toStrictEqual({
+    x: [2],
+    y: [2],
+  })
+})
