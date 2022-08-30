90a60ab7d04ba804e0b1f5886d91519cd9f0fbd0
TopazRN <topazsorowako2@gmail.com>
2022-08-30 20:28:31 +0700

    Add except parameter to penaltyOfMatrix

--- src/penaltyOfMatrix.ts
+++ src/penaltyOfMatrix.ts
@@ -5,14 +5,24 @@ export interface Penalty {
   y: number[]
 }
 
-export const penaltyOfMatrix = (matrix: number[][]): Penalty => {
+export const penaltyOfMatrix = (matrix: number[][], except?: Penalty): Penalty => {
   const penalty: Penalty = {
     x: [],
     y: [],
   }
 
   for (let y = 0; y < matrix.length; y++) {
-    const xs = matrix[y]
+    if (except && except?.y.indexOf(y) !== -1) {
+      continue
+    }
+    const xs: number[] = []
+    for (let x = 0; x < matrix[y].length; x++) {
+      if (except && except?.x.indexOf(x) !== -1) {
+        continue
+      }
+      const cost = matrix[y][x];
+      xs.push(cost)
+    }
     const min = indexOfMin(xs)
     const secondMin = indexOfMin(xs, [min])
     const difference = secondMin !== -1 ? xs[secondMin] - xs[min] : xs[min]
@@ -20,8 +30,14 @@ export const penaltyOfMatrix = (matrix: number[][]): Penalty => {
   }
 
   for (let x = 0; x < matrix[0].length; x++) {
+    if (except && except?.x.indexOf(x) !== -1) {
+      continue
+    }
     const ys: number[] = []
     for (let y = 0; y < matrix.length; y++) {
+      if (except && except?.y.indexOf(y) !== -1) {
+        continue
+      }
       const cost = matrix[y][x]
       ys.push(cost)
     }
