4eb7d68ef7ccc57e4cc572b0aa419aa449f21b33
TopazRN <topazsorowako2@gmail.com>
2022-08-30 20:29:33 +0700

    Create nextStep

--- src/nextStep.test.ts
+++ src/nextStep.test.ts
@@ -1,6 +1,16 @@
-import { stepOfProblem, type Step } from "./stepOfProblem"
+import { nextStep } from "./nextStep"
+import { Problem, type Step } from "./stepOfProblem"
 
 test("https://cdn1.byjus.com/wp-content/uploads/2022/04/Vogels-Approximation-Method-3.png", () => {
+  const problem: Problem = {
+    matrix: [
+      [3, 2, 7, 6],
+      [7, 5, 2, 3],
+      [2, 5, 4, 5],
+    ],
+    x: [60, 40, 20, 15],
+    y: [50, 60, 25],
+  }
   const input: Step = {
     matrix: [
       [0, 40, 0, 0],
@@ -37,5 +47,54 @@ test("https://cdn1.byjus.com/wp-content/uploads/2022/04/Vogels-Approximation-Met
     },
     total: "(40 x 2) + (10 x 3)",
   }
-  expect(stepOfProblem(input)).toStrictEqual(output)
+  expect(nextStep(problem, input)).toStrictEqual(output)
+})
+
+test("https://cdn1.byjus.com/wp-content/uploads/2022/04/Vogels-Approximation-Method-4.png", () => {
+  const problem: Problem = {
+    matrix: [
+      [3, 2, 7, 6],
+      [7, 5, 2, 3],
+      [2, 5, 4, 5],
+    ],
+    x: [60, 40, 20, 15],
+    y: [50, 60, 25],
+  }
+  const input: Step = {
+    matrix: [
+      [10, 40, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ],
+    x: [50, 0, 20, 15],
+    y: [0, 60, 25],
+    penalty: {
+      x: [1, 2, 2],
+      y: [3, 1, 2],
+    },
+    strike: {
+      x: [1],
+      y: [0],
+    },
+    total: "(40 x 2) + (10 x 3)",
+  }
+  const output: Step = {
+    matrix: [
+      [10, 40, 0, 0],
+      [0, 0, 0, 0],
+      [25, 0, 0, 0],
+    ],
+    x: [25, 0, 20, 15],
+    y: [0, 60, 0],
+    penalty: {
+      x: [5, 2, 2],
+      y: [1, 2],
+    },
+    strike: {
+      x: [1],
+      y: [0, 2],
+    },
+    total: "(40 x 2) + (10 x 3) + (25 x 2)",
+  }
+  expect(nextStep(problem, input)).toStrictEqual(output)
 })
---
+++ src/nextStep.ts
@@ -0,0 +1,46 @@
+import { indexOfMax } from "./indexOfMax";
+import { indexOfMin } from "./indexOfMin";
+import { penaltyOfMatrix } from "./penaltyOfMatrix";
+import { Problem, Step } from "./stepOfProblem";
+
+export const nextStep = (problem:Problem, step: Step) : Step => {
+  step = JSON.parse(JSON.stringify(step))
+
+  step.penalty = penaltyOfMatrix(problem.matrix, step.strike)
+  const xmax = indexOfMax(step.penalty.x, step.strike.x)
+  const ymax = indexOfMax(step.penalty.y, step.strike.y)
+  if (step.penalty.x[xmax] > step.penalty.y[ymax]) {
+    const ys: number[] = []
+    for (let y = 0; y < step.matrix.length; y++) {
+      const cost = problem.matrix[y][xmax]
+      ys.push(cost)
+    }
+    const ymin = indexOfMin(ys, step.strike.y)
+    const min = Math.min(step.y[ymin], step.x[xmax])
+    step.matrix[ymin][xmax] = min
+    step.y[ymin] -= min
+    step.x[xmax] -= min
+    if (step.y[ymin] === 0) {
+      step.strike.y.push(ymin)
+    }
+    if (step.x[xmax] === 0) {
+      step.strike.x.push(xmax)
+    }
+    step.total += ` + (${step.matrix[ymin][xmax]} x ${problem.matrix[ymin][xmax]})`
+  } else {
+    const xmin = indexOfMin(problem.matrix[ymax], step.strike.x)
+    const min = Math.min(step.y[ymax], step.x[xmin])
+    step.matrix[ymax][xmin] = min
+    step.y[ymax] -= min
+    step.x[xmin] -= min
+    if (step.y[ymax] === 0) {
+      step.strike.y.push(ymax)
+    }
+    if (step.x[xmin] === 0) {
+      step.strike.x.push(xmin)
+    }
+    step.total += ` + (${step.matrix[ymax][xmin]} x ${problem.matrix[ymax][xmin]})`
+  }
+
+  return step
+}
