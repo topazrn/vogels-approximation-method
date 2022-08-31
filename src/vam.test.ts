3d71c5577746fad510f19c3acc928784e66980d3
Topaz Rindu Nabiyallah <topaz.nabiyallah@binus.edu>
2022-08-31 10:25:28 +0700

    Add real test case to main function

--- src/vam.test.ts
+++ src/vam.test.ts
@@ -1,4 +1,4 @@
-import { Problem } from "./stepOfProblem"
+import { Problem, Step } from "./stepOfProblem"
 import { vam } from "./vam"
 
 test("https://cdn1.byjus.com/wp-content/uploads/2022/04/Vogels-Approximation-Method-2.png", () => {
@@ -11,5 +11,115 @@ test("https://cdn1.byjus.com/wp-content/uploads/2022/04/Vogels-Approximation-Met
     x: [60, 40, 20, 15],
     y: [50, 60, 25],
   }
-  expect(vam(problem)).toStrictEqual([])
+  const solution: Step[] = [
+    {
+      matrix: [
+        [0, 40, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ],
+      penalty: {
+        x: [1, 3, 2, 2],
+        y: [1, 1, 2],
+      },
+      strike: {
+        x: [1],
+        y: [],
+      },
+      total: "(40 x 2)",
+      x: [60, 0, 20, 15],
+      y: [10, 60, 25],
+    },
+    {
+      matrix: [
+        [10, 40, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ],
+      penalty: {
+        x: [1, 0, 2, 2],
+        y: [3, 1, 2],
+      },
+      strike: {
+        x: [1],
+        y: [0],
+      },
+      total: "(40 x 2) + (10 x 3)",
+      x: [50, 0, 20, 15],
+      y: [0, 60, 25],
+    },
+    {
+      matrix: [
+        [10, 40, 0, 0],
+        [0, 0, 0, 0],
+        [25, 0, 0, 0],
+      ],
+      penalty: {
+        x: [5, 0, 2, 2],
+        y: [0, 1, 2],
+      },
+      strike: {
+        x: [1],
+        y: [0, 2],
+      },
+      total: "(40 x 2) + (10 x 3) + (25 x 2)",
+      x: [25, 0, 20, 15],
+      y: [0, 60, 0],
+    },
+    {
+      matrix: [
+        [10, 40, 0, 0],
+        [25, 0, 0, 0],
+        [25, 0, 0, 0],
+      ],
+      penalty: {
+        x: [7, 0, 2, 3],
+        y: [0, 1, 0],
+      },
+      strike: {
+        x: [1, 0],
+        y: [0, 2],
+      },
+      total: "(40 x 2) + (10 x 3) + (25 x 2) + (25 x 7)",
+      x: [0, 0, 20, 15],
+      y: [0, 35, 0],
+    },
+    {
+      matrix: [
+        [10, 40, 0, 0],
+        [25, 0, 0, 15],
+        [25, 0, 0, 0],
+      ],
+      penalty: {
+        x: [0, 0, 2, 3],
+        y: [0, 1, 0],
+      },
+      strike: {
+        x: [1, 0, 3],
+        y: [0, 2],
+      },
+      total: "(40 x 2) + (10 x 3) + (25 x 2) + (25 x 7) + (15 x 3)",
+      x: [0, 0, 20, 0],
+      y: [0, 20, 0],
+    },
+    {
+      matrix: [
+        [10, 40, 0, 0],
+        [25, 0, 20, 15],
+        [25, 0, 0, 0],
+      ],
+      penalty: {
+        x: [0, 0, 2, 0],
+        y: [0, 2, 0],
+      },
+      strike: {
+        x: [1, 0, 3, 2],
+        y: [0, 2, 1],
+      },
+      total: "(40 x 2) + (10 x 3) + (25 x 2) + (25 x 7) + (15 x 3) + (20 x 2)",
+      x: [0, 0, 0, 0],
+      y: [0, 0, 0],
+    },
+  ]
+  expect(vam(problem)).toStrictEqual(solution)
 })
