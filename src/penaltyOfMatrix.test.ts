a5b91dddd524c546f7ef96b90c7080c5e99aa5a5
TopazRN <topazsorowako2@gmail.com>
2022-08-31 08:09:20 +0700

    Change format of input and output

--- src/penaltyOfMatrix.test.ts
+++ src/penaltyOfMatrix.test.ts
@@ -2,11 +2,17 @@ import { penaltyOfMatrix } from "./penaltyOfMatrix"
 
 test("https://cdn1.byjus.com/wp-content/uploads/2022/04/Vogels-Approximation-Method-1.png", () => {
   expect(
-    penaltyOfMatrix([
-      [3, 2, 7, 6],
-      [7, 5, 2, 3],
-      [2, 5, 4, 5],
-    ])
+    penaltyOfMatrix(
+      [
+        [3, 2, 7, 6],
+        [7, 5, 2, 3],
+        [2, 5, 4, 5],
+      ],
+      {
+        x: [],
+        y: [],
+      }
+    )
   ).toStrictEqual({
     x: [1, 3, 2, 2],
     y: [1, 1, 2],
@@ -15,58 +21,95 @@ test("https://cdn1.byjus.com/wp-content/uploads/2022/04/Vogels-Approximation-Met
 
 test("https://cdn1.byjus.com/wp-content/uploads/2022/04/Vogels-Approximation-Method-2.png", () => {
   expect(
-    penaltyOfMatrix([
-      [3, 7, 6],
-      [7, 2, 3],
-      [2, 4, 5],
-    ])
+    penaltyOfMatrix(
+      [
+        [3, 2, 7, 6],
+        [7, 5, 2, 3],
+        [2, 5, 4, 5],
+      ],
+      {
+        x: [1],
+        y: [],
+      }
+    )
   ).toStrictEqual({
-    x: [1, 2, 2],
+    x: [1, 0, 2, 2],
     y: [3, 1, 2],
   })
 })
 
 test("https://cdn1.byjus.com/wp-content/uploads/2022/04/Vogels-Approximation-Method-3.png", () => {
   expect(
-    penaltyOfMatrix([
-      [7, 2, 3],
-      [2, 4, 5],
-    ])
+    penaltyOfMatrix(
+      [
+        [3, 2, 7, 6],
+        [7, 5, 2, 3],
+        [2, 5, 4, 5],
+      ],
+      {
+        x: [1],
+        y: [0],
+      }
+    )
   ).toStrictEqual({
-    x: [5, 2, 2],
-    y: [1, 2],
+    x: [5, 0, 2, 2],
+    y: [0, 1, 2],
   })
 })
 
 test("https://cdn1.byjus.com/wp-content/uploads/2022/04/Vogels-Approximation-Method-4.png", () => {
   expect(
-    penaltyOfMatrix([
-      [7, 2, 3],
-    ])
+    penaltyOfMatrix(
+      [
+        [3, 2, 7, 6],
+        [7, 5, 2, 3],
+        [2, 5, 4, 5],
+      ],
+      {
+        x: [1],
+        y: [0, 2],
+      }
+    )
   ).toStrictEqual({
-    x: [7, 2, 3],
-    y: [1],
+    x: [7, 0, 2, 3],
+    y: [0, 1, 0],
   })
 })
 
 test("https://cdn1.byjus.com/wp-content/uploads/2022/04/Vogels-Approximation-Method-5.png", () => {
   expect(
-    penaltyOfMatrix([
-      [2, 3],
-    ])
+    penaltyOfMatrix(
+      [
+        [3, 2, 7, 6],
+        [7, 5, 2, 3],
+        [2, 5, 4, 5],
+      ],
+      {
+        x: [1, 0],
+        y: [0, 2],
+      }
+    )
   ).toStrictEqual({
-    x: [2, 3],
-    y: [1],
+    x: [0, 0, 2, 3],
+    y: [0, 1, 0],
   })
 })
 
 test("https://cdn1.byjus.com/wp-content/uploads/2022/04/Vogels-Approximation-Method-6.png", () => {
   expect(
-    penaltyOfMatrix([
-      [2],
-    ])
+    penaltyOfMatrix(
+      [
+        [3, 2, 7, 6],
+        [7, 5, 2, 3],
+        [2, 5, 4, 5],
+      ],
+      {
+        x: [1, 0, 3],
+        y: [0, 2],
+      }
+    )
   ).toStrictEqual({
-    x: [2],
-    y: [2],
+    x: [0, 0, 2, 0],
+    y: [0, 2, 0],
   })
 })
--- src/penaltyOfMatrix.ts
+++ src/penaltyOfMatrix.ts
@@ -5,29 +5,51 @@ export interface Penalty {
   y: number[]
 }
 
-export const penaltyOfMatrix = (matrix: number[][]): Penalty => {
+export const penaltyOfMatrix = (matrix: number[][], except: Penalty): Penalty => {
   const penalty: Penalty = {
     x: [],
     y: [],
   }
 
   for (let y = 0; y < matrix.length; y++) {
+    if (except && except.y.indexOf(y) !== -1) {
+      penalty.y.push(0)
+      continue
+    }
     const xs = matrix[y]
-    const min = indexOfMin(xs)
-    const secondMin = indexOfMin(xs, [min])
-    const difference = secondMin !== -1 ? xs[secondMin] - xs[min] : xs[min]
+    const min = indexOfMin(xs, except.x)
+    const secondMin = indexOfMin(xs, [min, ...except.x])
+    let difference = 0
+    if (secondMin !== -1 && min !== -1) {
+      difference = xs[secondMin] - xs[min]
+    } else if (secondMin !== -1) {
+      difference = xs[secondMin]
+    } else if (min !== -1) {
+      difference = xs[min]
+    }
     penalty.y.push(difference)
   }
 
   for (let x = 0; x < matrix[0].length; x++) {
+    if (except && except.x.indexOf(x) !== -1) {
+      penalty.x.push(0)
+      continue
+    }
     const ys: number[] = []
     for (let y = 0; y < matrix.length; y++) {
       const cost = matrix[y][x]
       ys.push(cost)
     }
-    const min = indexOfMin(ys)
-    const secondMin = indexOfMin(ys, [min])
-    const difference = secondMin !== -1 ? ys[secondMin] - ys[min] : ys[min]
+    const min = indexOfMin(ys, except.y)
+    const secondMin = indexOfMin(ys, [min, ...except.y])
+    let difference = 0
+    if (secondMin !== -1 && min !== -1) {
+      difference = ys[secondMin] - ys[min]
+    } else if (secondMin !== -1) {
+      difference = ys[secondMin]
+    } else if (min !== -1) {
+      difference = ys[min]
+    }
     penalty.x.push(difference)
   }
 
